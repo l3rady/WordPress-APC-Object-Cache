aad50cf4bf40839ba66e565e0547a9c302f1b15d
Scott Cariss <scott@bigfish.co.uk>
2014-07-02 14:57:36 +0100

    fix logic in get cache version

--- object-cache.php
+++ object-cache.php
@@ -763,7 +763,7 @@ class WP_Object_Cache {
 	 * @return int cache version
 	 */
 	private function _get_cache_version( $key ) {
-		if ( !$this->apc_available ) {
+		if ( $this->apc_available ) {
 			$version = (int) apc_fetch( $key );
 		}
 		elseif ( isset( $this->non_persistent_cache[$key] ) ) {
